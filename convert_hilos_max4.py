9ab064cc70ecf6aa7d1c93692d9ed5b8e1c96f6e
Dini <leandrodini@lakaut.com.ar>
2025-01-08 11:35:28 -0300

    first commit

---
+++ convert_hilos_max4.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+input_folder = r"C:/Users/Ldini/Documents/Tesseract/app/input"
+output_folder = r"C:/Users/Ldini/Documents/Tesseract/app/output"
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+pdf_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.pdf')]
+
+def process_pdf(pdf_file):
+    input_path = os.path.join(input_folder, pdf_file)
+    output_file = f"ocr_{pdf_file}"  
+    output_path = os.path.join(output_folder, output_file)
+    
+    command = [
+        "docker", "run", "--rm",
+        "-v", f"{input_folder}:/data/input", 
+        "-v", f"{output_folder}:/data/output",  
+        "jbarlow83/ocrmypdf",
+        f"/data/input/{pdf_file}",
+        f"/data/output/{output_file}",
+        "--force-ocr",
+        "--image-dpi", "200", 
+        "--jobs",
+        "4"
+    ]
+ 
+    print(f"Procesando: {pdf_file}")
+    try:
+       
+        subprocess.run(command, check=True)
+        print(f"Archivo procesado: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error al procesar {pdf_file}: {e}")
+
+
+max_threads = 4
+with ThreadPoolExecutor(max_threads) as executor:
+    executor.map(process_pdf, pdf_files)
+
+print("Procesamiento completado.")
